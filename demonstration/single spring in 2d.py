0073121297ad95b70b79e2c866e9098c22169c8e
Elliott Perryman <elliottperryman@vivaldi.net>
2020-03-02 09:30:02 -0800

    cleaning demonstration

--- demonstration/single spring in 2d.py
+++ demonstration/single spring in 2d.py
@@ -49,30 +49,5 @@ plot(R, D[3:])
 # In[ ]:
 
 
-def Energy(R, springs, displacements):
-    E = 0
-    for i, (start,stop) in enumerate(springs):
-        v = R[stop] - R[start]
-        v /= np.linalg.norm(v)
-        
-        stretch = displacements[stop] - displacements[start]
-        
-        E += .5 * (stretch @ v)**2
-    return E
-
-
-# In[ ]:
-
-
-displacements = np.array([
-    [-0.5, 0.5],
-    [0.5, -0.5]
-])
-Energy(R, springs, displacements)
-
-
-# In[ ]:
-
-
 
 
